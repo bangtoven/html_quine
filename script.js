b8475f2b325473dbf439a3a8e93d522d40ced364
Jungho Bang <me@bangtoven.com>
2025-12-11 01:33:01 -0800

    reduce redundancy
    min diff

--- script.js
+++ script.js
@@ -3,14 +3,28 @@ document.addEventListener('DOMContentLoaded', async () => {
   if (nav) nav.outerHTML = await (await fetch('nav.html')).text();
   
   document.querySelectorAll('img').forEach(img => {
-    if (img.closest('figure[src]')) return;
+    if (img.closest('figure[data-src]')) return;
     const figure = document.createElement('figure');
-    figure.setAttribute('src', img.getAttribute('src'));
+    figure.setAttribute('data-src', img.getAttribute('src'));
     img.parentNode.insertBefore(figure, img);
     figure.appendChild(img);
   });
 
+  const taggable = [
+    'html','head','title','meta','link','script','body','nav',
+    'h1','h2','p','pre','code','a','em','strong',
+    'div','ul','li'
+  ];
+  document.querySelectorAll(taggable.join(',')).forEach(el => {
+    el.setAttribute('data-tag', el.tagName.toLowerCase());
+    const attrs = Array.from(el.attributes)
+      .filter(({ name }) => name !== 'data-tag' && name !== 'data-attrs')
+      .map(({ name, value }) => value ? `${name}='${value}'` : name)
+      .join(' ');
+    if (attrs) el.setAttribute('data-attrs', ` ${attrs}`);
+  });
+
   requestAnimationFrame(() => {
-    document.body.scrollIntoView({ behavior: 'auto', block: 'start' })
+    document.body.scrollIntoView({ behavior: 'auto', block: 'start' });
   });
 });
