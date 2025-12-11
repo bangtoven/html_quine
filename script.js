d15987986c9efd6d36ac36738af95a9399b329bd
Jungho Bang <me@bangtoven.com>
2025-12-10 18:04:35 -0800

    nav
    min diff

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+  const nav = document.querySelector('.nav');
+  if (nav) nav.outerHTML = await (await fetch('nav.html')).text();
+  
   document.querySelectorAll('img').forEach(img => {
     if (img.closest('figure[src]')) return;
     const figure = document.createElement('figure');
@@ -6,5 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
     img.parentNode.insertBefore(figure, img);
     figure.appendChild(img);
   });
-  requestAnimationFrame(() => document.body.scrollIntoView({ behavior: 'auto', block: 'start' }));
+
+  requestAnimationFrame(() => {
+    document.body.scrollIntoView({ behavior: 'auto', block: 'start' })
+  });
 });
